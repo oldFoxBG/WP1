96a3024d1f5adcd692cb1d13e18c3a8e05631c9c
oldFoxBG <92375637+oldFoxBG@users.noreply.github.com>
2025-02-26 12:29:40 +0100

    Test Git Repo
    Test Git Repo: edit file on fit, pull it from Eclipse

--- wp-content/plugins/test-payment/includes/main-file.php
+++ wp-content/plugins/test-payment/includes/main-file.php
@@ -13,5 +13,14 @@ if (!defined('ABSPATH')) {
 
 class Test_Payment_Gateway extends WC_Payment_Gateway_CC
 {
-
+/**
+ * /**
+ * TEST Payment Gateway
+ * 
+ * @author Author Name
+ * @since 1.0.0
+ */
+ * 
+ */
+    
 }
